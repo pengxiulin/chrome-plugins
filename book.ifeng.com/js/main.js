469b1637f1759b8f0019af2db222ea5d50f07df0
pengxiulin <pengxiulin@live.com>
2013-02-12 21:03:49 +0800

    去jQuery、完成链接点击事件和请求

---
+++ book.ifeng.com/js/main.js
@@ -0,0 +1,34 @@
+var loadArticle = function(b,c){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+	if(xhr.readyState==4){
+	    if(xhr.status==200){
+		var response = JSON.parse(xhr.responseText);
+		showContent(response.content);
+	    }
+	}
+    }
+    xhr.open("POST","http://v.book.ifeng.com/book/remc.htm",true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send("b="+b+"&c="+c);
+}
+var clickHandler = function(e){
+    var c = e.target.href;
+    c = c.substring(c.indexOf(b)+b.length+1,c.indexOf(".htm"));
+    loadArticle(b,c);
+    e.preventDefault();
+}
+l = location.href;//为了简化后续字符操作而做的变量名称简化
+var b = l.substring(l.indexOf(l.match(/\d*.htm/)[0]),l.indexOf(".htm"));
+var elems_a = document.querySelectorAll(".book_neirong_left1 li a");
+var elem_length = elems_a.length;
+var temp_elem;
+for(var i=0;i<elem_length;i++){
+    temp_elem = elems_a[i];
+    temp_elem.addEventListener("click",clickHandler,false);
+}
+
+function showContent(content){
+    // TODO 将这里内容以弹出层展示
+    console.log(content);
+}
